00ba8fe04666b328b158c47311010f532b465043
Muhammad Abdullah <113471263+abdullah-dev5@users.noreply.github.com>
2023-11-25 00:49:47 +0500

    NotFound Animation Added

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import Lottie from 'lottie-react';
-
+import AnimationNotFound from './assets/AnimationNotFound.json'
 function NotFound() {
   return (
     <div className='hero flex-1-1-auto flex-col justify-center '>
       <div className="m-2 my-20 text-center hero-content flex justify-center items-center">
         <div className="max-w-lg">
-        <h1 className="text-8xl font-bold mb-8">
-          Oops!
-        </h1>
-        <p className="text-5xl mb-8">
-          404 - Page not found!
-        </p>
+       <Lottie   animationData = {AnimationNotFound}/>
         </div>
       </div>
     </div>
